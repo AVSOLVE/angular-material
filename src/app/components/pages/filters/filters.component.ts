f4a039ff911c7e5cbd0965a3c6fc72ddf4d7e660
Alexandre Valdivino <alexandre.valdivino@macrolabs.com.br>
2023-11-21 00:14:58 -0300

    teste

--- src/app/components/pages/filters/filters.component.ts
+++ src/app/components/pages/filters/filters.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
-  templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.css']
+  templateUrl: './filters.component.html'
 })
 export class FiltersComponent {
-
+ categories = ['shoes', 'sports', 'beach'];
 }
